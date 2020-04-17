05b181524c72e302078be2edc01969de4b64a907
Tim Shaker <tim@tmshkr.com>
2020-04-17 08:15:31 -0700

    server.js

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const server = express();
-
-server.use(express.json());
-server.use(helmet());
-server.use(cors());
-
-server.get("*", (req, res) => res.json({ server: "running..." }));
+const server = require("./server");
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.get("*", (req, res) => res.json({ server: "running..." }));
+
+module.exports = server;
