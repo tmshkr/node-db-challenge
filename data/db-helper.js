8963d8d97b04e6bbf722fd9725db20d9d79fb4db
Tim Shaker <tim@tmshkr.com>
2020-04-17 08:48:03 -0700

    set up db functions

---
+++ data/db-helper.js
@@ -0,0 +1,19 @@
+const db = require("./db-config");
+
+module.exports = {
+  addProject,
+  getProjects,
+  addTask,
+  getTasks,
+  addResource,
+  getResources,
+};
+
+function addProject() {}
+function getProjects() {}
+
+function addTask() {}
+function getTasks() {}
+
+function addResource() {}
+function getResources() {}
